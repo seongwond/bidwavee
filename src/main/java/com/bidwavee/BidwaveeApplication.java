3e22a217a1eb26049070ae5ae3d02d020bdedddd
Min Seongwon <mks484414@gmail.com>
2025-06-08 19:17:22 +0900

    feat:프로젝트 생성

---
+++ src/main/java/com/bidwavee/BidwaveeApplication.java
@@ -0,0 +1,13 @@
+package com.bidwavee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidwaveeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidwaveeApplication.class, args);
+    }
+
+}
